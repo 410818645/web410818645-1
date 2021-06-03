2adc0fc96b46617b771702acee079cb97cb30892
譚靜雯 <annietan900419@gmail.com>
2021-06-03 21:25:32 +0800

    all1

--- Web410818645/ViewModels/SignUpData.cs
+++ Web410818645/ViewModels/SignUpData.cs
@@ -7,5 +7,18 @@ namespace Web410818645.ViewModels
 {
     public class SignUpData
     {
+        public string Name { get; set; }
+
+        public string Account { get; set; }
+
+        public string Password { get; set; }
+
+        public string NameMessage { get; set; }
+
+        public string AccountMessage { get; set; }
+
+        public string PasswordMessage { get; set; }
+
+        public string Message { get; set; }
     }
 }
